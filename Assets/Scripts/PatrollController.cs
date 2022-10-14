d827e7326bc6880e484cdb416adc25168ea0b8cf
Golipeg <golipeg@gmail.com>
2022-10-14 22:45:25 +0300

    Rewrite code PatrollController.cs

--- Assets/Scripts/PatrollController.cs
+++ Assets/Scripts/PatrollController.cs
@@ -11,39 +11,46 @@ public class PatrollController : MonoBehaviour
 {
     [SerializeField] private float _speed; // отвечает за скорость передвижения
     [SerializeField] private Transform[] _points; // массив точек, по которым будет передвигаться объект
-    private int _point; // номер точки к которой будет идти наш объект
-    private float _waitTime;
-    [SerializeField] private float _startWaitTime;
+    private Vector3 _startPosition;// точка старта
+    private Vector3 _endPosition;
+    [SerializeField] float _pauseDuration=2f;
+    private float _pauseBetweenMovingToNextPoint;//пауза перед началом движения к следующей точке
+    private float _arrivalDuration;// длительность пути от точки до точки
+    private int _currentPointIndex;// наш текущий индекс
     private float _currentTime;
-    private void Start()
+
+    private void Awake()
     {
-        _waitTime = _startWaitTime;
-        _point = 1;
+        SetNextPoint();
+    }
+
+    private void SetNextPoint()
+    {
+        var nextPointIndex = (_currentPointIndex + 1) % _points.Length;// узнаем номер следующей точки, куда пойдёт наш объект.
+        _startPosition = _points[_currentPointIndex].position;
+        _endPosition = _points[nextPointIndex].position;
+        /// узнаем расстояние между точек 
+        _arrivalDuration = Vector3.Distance(_startPosition, _endPosition) / _speed;// делим на speed т.к. Distance должна быть от 0 до 1;
+        _currentPointIndex = nextPointIndex;
     }
 
     private void Update()
-    {
-       // _currentTime += Time.deltaTime;
-        //var progress = _currentTime / _speed;
-        
-        transform.position = Vector3.Lerp(transform.position, _points[_point].position, _speed*Time.deltaTime);
-        if (Vector3.Distance(transform.position, _points[_point].position) < 0.2f)
+    {///
+     /// сгенерили паузу между передвижением к след.точки
+     
+        _pauseBetweenMovingToNextPoint += Time.deltaTime;
+        if ( _pauseBetweenMovingToNextPoint<= _pauseDuration)
         {
-            if (_waitTime <= 0)
-            {
-                _point++;
-                _waitTime = _startWaitTime;
-            }
-            else
-            {
-                _waitTime -= Time.deltaTime;
-            }
-
-            if (_point == _points.Length)
-            {
-                _point = 0;
-                //transform.position = _points[_point].position;
-            }
+            return;
+        }
+        _currentTime += Time.deltaTime;
+        var progress = _currentTime / _arrivalDuration;
+        transform.position = Vector3.Lerp(_startPosition, _endPosition, progress);
+        if (_currentTime > _arrivalDuration)
+        {
+            _currentTime = 0f;
+            _pauseBetweenMovingToNextPoint = 0f;
+            SetNextPoint();
         }
     }
 }
